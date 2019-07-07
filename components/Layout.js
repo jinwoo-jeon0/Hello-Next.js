55f5788d3a988d6474f0a0c832ae4449356142db
Jinwoo Jeon <jinwoo.jeon0@gmail.com>
2019-07-07 20:26:06 +0900

    Give styles

--- components/Layout.js
+++ components/Layout.js
@@ -1,7 +1,13 @@
 import Header from '../components/Header';
 
+const layoutStyle = {
+  margin: 20,
+  padding: 20,
+  border: '1px solid #DD',
+};
+
 const Layout = props => (
-  <div>
+  <div style={layoutStyle}>
     <Header />
     {props.children}
   </div>
