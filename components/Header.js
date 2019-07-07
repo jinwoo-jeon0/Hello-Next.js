c975aa821af2a49200a263ec761618ab9bed18c6
Jinwoo Jeon <jinwoo.jeon0@gmail.com>
2019-07-07 20:12:22 +0900

    Add a link to home

--- components/Header.js
+++ components/Header.js
@@ -2,6 +2,9 @@ import Link from 'next/link';
 
 const Header = () => (
   <div>
+    <Link href="/">
+      <a title="Home">Home</a>
+    </Link>
     <Link href="/about">
       <a title="About Page">About Page</a>
     </Link>
