5d56cc59a337678b5a4f7def22d188ed1dfa288c
Jinwoo Jeon <jinwoo.jeon0@gmail.com>
2019-07-28 23:32:36 +0900

    Extract Content()

--- pages/post.js
+++ pages/post.js
@@ -1,13 +1,19 @@
 import { useRouter } from 'next/router';
 import Layout from "../components/Layout";
 
-const Page = () => {
+const Content = () => {
   const router = useRouter();
-
   return (
-    <Layout>
+    <>
       <h1>{router.query.title}</h1>
       <p>This is the blog post content.</p>
+    </>
+  );
+}
+const Page = () => {
+  return (
+    <Layout>
+      <Content />
     </Layout>
   );
 };
